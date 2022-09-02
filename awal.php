32cb91cd02f6eae23349ffbe38a2b3f350bbc9d4
laila <namengnjilih.lailawae@gamail.com>
2022-09-02 12:36:45 +0700

    lail

--- awal.php
+++ awal.php
@@ -582,7 +582,7 @@ include "koneksi.php";
 
   </html>   
 
-                </div>
+      </div>
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
